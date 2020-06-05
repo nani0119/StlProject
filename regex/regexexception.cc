1269a23248efe71a26137505fffe1bb30deaeb09
zhaorj <zhaorj@neusoft.com>
2020-06-05 15:54:37 +0800

    add regex exception example

---
+++ regex/regexexception.cc
@@ -0,0 +1,20 @@
+#include <regex>
+#include <iostream>
+#include "regexstuff.h"
+
+using namespace std;
+int main()
+{
+    try
+    {
+        // initialize regular expression with invalid syntax:
+        regex pat("\\\\.*index\\{([^}]*)\\}",
+                  regex_constants::grep | regex_constants::icase);
+    }
+    catch (const regex_error &e)
+    {
+        cerr << "regex_error: \n"
+             << " what(): " << e.what() << "\n"
+             << " code(): " << regexCode(e.code()) << endl;
+    }
+}
